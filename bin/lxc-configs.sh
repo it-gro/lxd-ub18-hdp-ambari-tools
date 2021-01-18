5a16ad70954301fe740eb86e27426ccf7e0a594a
it-gro <13005925+it-gro@users.noreply.github.com>
2021-01-18 10:20:45 +0100

    wip

--- bin/lxc-configs.sh
+++ bin/lxc-configs.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # 2019-03, Bruno Grossniklaus, https://github.com/it-gro
 
-for lxid in $(lxc list ${cl_name} -cn --format csv); do 
+for lxid in $(lxc list ${cl_name} -cn --format csv); do
   echo $lxid
   lxc config show $lxid | grep -E 'limits|boot'
 done
 
+echo default profile
 lxc profile show default | grep limit
